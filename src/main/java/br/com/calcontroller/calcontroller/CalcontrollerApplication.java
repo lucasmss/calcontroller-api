7723ef8c779f1d6062130960fadd808d806f093d
lucas mateus <lucasmss16@gmail.com>
2023-02-10 07:53:00 -0300

    Criacao do projeto calcontroller api

---
+++ src/main/java/br/com/calcontroller/calcontroller/CalcontrollerApplication.java
@@ -0,0 +1,15 @@
+package br.com.calcontroller.calcontroller;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CalcontrollerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CalcontrollerApplication.class, args);
+
+		System.out.println("hELLOW WORD SPRING");
+	}
+
+}
---
+++ src/test/java/br/com/calcontroller/calcontroller/CalcontrollerApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.calcontroller.calcontroller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CalcontrollerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
